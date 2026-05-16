65411849ede24b823743989f432c23702f6881e7
benyue <1162572391@qq.com>
2026-05-16 15:47:52 +0800

    assignment 1/3/4

---
+++ a4/student/src/london_baseline.py
@@ -0,0 +1,25 @@
+# TODO: [part d]
+# Calculate the accuracy of a baseline that simply predicts "London" for every
+#   example in the dev set.
+# Hint: Make use of existing code.
+# Your solution here should only be a few lines.
+
+import argparse
+import utils
+
+def main():
+    accuracy = 0.0
+
+    # Compute accuracy in the range [0.0, 100.0]
+    ### YOUR CODE HERE ###
+    prediction = ["London"] * 500
+    total, correct = utils.evaluate_places("../birth_dev.tsv", prediction)
+    accuracy = correct / total * 100
+    ### END YOUR CODE ###
+
+    return accuracy
+
+if __name__ == '__main__':
+    accuracy = main()
+    with open("london_baseline_accuracy.txt", "w", encoding="utf-8") as f:
+        f.write(f"{accuracy}\n")
